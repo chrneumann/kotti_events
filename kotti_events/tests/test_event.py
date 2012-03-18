cde4df48a3186840025e7d599d294d6fc30a7a74
Christian Neumann <cneumann@datenkarussell.de>
2012-03-18 15:15:25 +0100

    Added tests for Event

---
+++ kotti_events/tests/test_event.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from kotti.testing import UnitTestBase
+
+class TestEventPictureMethods(UnitTestBase):
+    """
+    Test methods which operato on the event's pictures.
+    """
+    def setUp(self):
+        from kotti_events.resources import Event
+        self.event = Event(u"Frankfurt", u"<b>Awesome!</b>",
+                           datetime.now, None, None, None)
+
+        from kotti_events.resources import EventPicture
+        for name in ['foo', 'bar', 'cruz']:
+            self.event[name] = EventPicture(name=name, mimetype=u'image/jpeg')
+
+        self.emty_event = Event(u"Frankfurt", u"<b>Awesome!</b>",
+                                datetime.now, None, None, None)
+                          
+    def test_get_icon(self):
+        icon = self.event.get_icon()
+        assert icon.name == 'foo'
+
+        icon = self.emty_event.get_icon()
+        assert icon == None
+
+    def test_get_pictures(self):
+        pictures = list(self.event.get_pictures())
+        assert len(pictures) == 3
+        assert pictures[2].name == 'cruz'
+
+        pictures = list(self.emty_event.get_pictures())
+        assert len(pictures) == 0
