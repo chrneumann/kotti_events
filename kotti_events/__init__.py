564bc0413574d7f2e41d5a2666326c9e05ab9540
Christian Neumann <cneumann@datenkarussell.de>
2012-07-29 12:11:30 +0200

    Fix deprecated use of kotti.includes.

--- kotti_events/__init__.py
+++ kotti_events/__init__.py
@@ -1,5 +1,5 @@
 def kotti_configure(settings):
-    settings['kotti.includes'] += ' kotti_events.views'
+    settings['pyramid.includes'] += ' kotti_events.views'
     settings['kotti.available_types'] += ' kotti_events.resources.EventFolder'
     settings['kotti.available_types'] += ' kotti_events.resources.Event'
     settings['kotti.available_types'] += ' kotti_events.resources.EventPicture'
